8d0baf721849372fa2617f26173e74d4bde4d91b
adi00 <adiballia0@gmail.com>
2025-03-23 15:19:26 +0530

    ddd

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = 5000;
+const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static("public"));git
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
