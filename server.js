a1e7f8667a88042a66ab48cdb048dbe0c79caacd
adi00 <adiballia0@gmail.com>
2025-03-23 14:10:35 +0530

    Port changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = 5050;
+const PORT = 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
